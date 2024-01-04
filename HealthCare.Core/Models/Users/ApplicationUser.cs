d3b28b4224d5c0b47f01418933eb02a9252fafb1
rickardthunborg <rickard.a.thunborg@gmail.com>
2024-01-04 14:21:18 +0100

    initial commit

--- HealthCare.Core/Models/Users/ApplicationUser.cs
+++ HealthCare.Core/Models/Users/ApplicationUser.cs
@@ -7,6 +7,17 @@ namespace HealthCare.Core.Models.Users
         public UserType Type { get; set; } = UserType.Patient;
 
         public List<Appointment> Appointments { get; set; }
+
+        public static List<ApplicationUser> GenerateMockDoctors()
+        {
+            // Mock data: List of doctors for demonstration
+            return new List<ApplicationUser>
+            {
+                new ApplicationUser { UserName = "DrSmith" },
+                new ApplicationUser { UserName = "DrJohnson" },
+                // Add more doctors as needed
+            };
+        }
     }
 
     public enum UserType
